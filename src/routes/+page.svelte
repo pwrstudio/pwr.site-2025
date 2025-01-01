3e036c1bb86bb8d7546dc5883e478ac1c99e30b2
PWR <pwrpntr@gmail.com>
2025-01-01 17:52:22 +0100

    add cottonmouth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,17 +18,17 @@
   <p>
     – collaborators and clients
     <br />
-    Amnesia Scanner / AQNB / Balenciaga / Bauhaus-Universität, Weimar / Beirut Art
-    Center / Berghain / Black Swan / Dorota Gawęda and Eglė Kulbokaitė / Dover Street
-    Market / drain gang / Guggenheim Bilbao / Guild / Hamburger Bahnhof / Haus der
-    Kunst München / HDK Göteborg / HfG Karlsruhe / Holly Herndon and Mat Dryhurst
-    / Jenna Sutela / Jonatan Leandoer12 / Kablam / Kaleidoscope / Katja Novitskova
-    / Kunsthalle Basel / Malmö Konsthall / Moving Castles / Museum für Moderne Kunst
-    Frankfurt am Main / Naturkunde Museum Berlin / Nemesis / Nike / Nina protocol
-    / Novembre / OEI / PAN Records / P Staff / Ravecon0 / Research Center for Proxy
-    Politics / Rhizome / Ringier / Sharjah Architecture Triennial / Spazio Maiocchi
-    / Städelschule / Taschen / TLTR Press / Transmediale / Trust / Ullens Center
-    for Contemporary Art Beijing / Uniswap / Vibram / World Affairs / Yung Lean /
-    Zak Group / 0xParc
+    Amnesia Scanner / AQNB / Balenciaga / Bauhaus-Universität Weimar / Beirut Art
+    Center / Berghain / Black Swan / cottonmouth / Dorota Gawęda and Eglė Kulbokaitė
+    / Dover Street Market / drain gang / Guggenheim Bilbao / Guild / Hamburger Bahnhof
+    / Haus der Kunst München / HDK Göteborg / HfG Karlsruhe / Holly Herndon and Mat
+    Dryhurst / Jenna Sutela / Jonatan Leandoer12 / Kablam / Kaleidoscope / Katja
+    Novitskova / Kunsthalle Basel / Malmö Konsthall / Moving Castles / Museum für
+    Moderne Kunst Frankfurt am Main / Naturkunde Museum Berlin / Nemesis / Nike /
+    Nina protocol / Novembre / OEI / PAN Records / P Staff / Ravecon0 / Research
+    Center for Proxy Politics / Rhizome / Ringier / Sharjah Architecture Triennial
+    / Spazio Maiocchi / Städelschule / Taschen / TLTR Press / Transmediale / Trust
+    / Ullens Center for Contemporary Art Beijing / Uniswap / Vibram / World Affairs
+    / Yung Lean / Zak Group / 0xParc
   </p>
 </div>
