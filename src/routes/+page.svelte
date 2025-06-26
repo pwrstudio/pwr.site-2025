c5642b097e43189d02521c8a77f90f9c277984f0
PWR <pwrpntr@gmail.com>
2025-06-26 14:03:44 +0200

    update content and remove mis-commited dirs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
     <br />
     <a href="tel:004917631664276">+49(0)17631664276</a>
     <br />
-    Kluckstrasse 25, 10785 Berlin
+    Leuschnerdamm 13, 10999 Berlin
     <br />
     <a href="http://are.na/pwr-studio" target="_blank">are.na/pwr-studio</a>
     <br />
@@ -20,15 +20,15 @@
     <br />
     Amnesia Scanner / AQNB / Balenciaga / Bauhaus-Universität Weimar / Beirut Art
     Center / Berghain / Black Swan / cottonmouth / Dorota Gawęda and Eglė Kulbokaitė
-    / Dover Street Market / drain gang / Guggenheim Bilbao / Guild / Hamburger Bahnhof
-    / Haus der Kunst München / HDK Göteborg / HfG Karlsruhe / Holly Herndon and Mat
-    Dryhurst / Jenna Sutela / Jonatan Leandoer12 / Kablam / Kaleidoscope / Katja
-    Novitskova / Kunsthalle Basel / Malmö Konsthall / Moving Castles / Museum für
-    Moderne Kunst Frankfurt am Main / Naturkunde Museum Berlin / Nemesis / Nike /
-    Nina protocol / Novembre / OEI / PAN Records / P Staff / Ravecon0 / Research
-    Center for Proxy Politics / Rhizome / Ringier / Sharjah Architecture Triennial
-    / Spazio Maiocchi / Städelschule / Taschen / TLTR Press / Transmediale / Trust
-    / Ullens Center for Contemporary Art Beijing / Uniswap / Vibram / World Affairs
-    / Yung Lean / Zak Group / 0xParc
+    / Dover Street Market / drain gang / Fondazione Prada / Guggenheim Bilbao / Guild
+    / Hamburger Bahnhof / Haus der Kunst München / HDK Göteborg / HfG Karlsruhe /
+    Holly Herndon and Mat Dryhurst / Jenna Sutela / Jonatan Leandoer12 / Kablam /
+    Kaleidoscope / Katja Novitskova / Kunsthalle Basel / Malmö Konsthall / Moving
+    Castles / Museum für Moderne Kunst Frankfurt am Main / Naturkunde Museum Berlin
+    / Nemesis / Nike / Nina protocol / Novembre / OEI / PAN Records / P Staff / Ravecon0
+    / Research Center for Proxy Politics / Rhizome / Ringier / Sharjah Architecture
+    Triennial / Spazio Maiocchi / Städelschule / Taschen / TLTR Press / Transmediale
+    / Trust / Ullens Center for Contemporary Art Beijing / Uniswap / Vibram / World
+    Affairs / Yung Lean / Zak Group / 0xParc
   </p>
 </div>
